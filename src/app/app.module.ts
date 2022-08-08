0eaa407571d9336fe0297c7e9abd4957494d1042
Shinobirun <adrian.latorre@gmail.com>
2022-08-08 16:33:02 -0300

    intarface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +13,8 @@ import { TaskComponent } from './components/task/task.component';
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    TaskComponent
+    TaskComponent,
+
   ],
   imports: [
     BrowserModule,
