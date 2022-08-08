250f4991f1245b958d4169d3b168fa370fd43242
Shinobirun <adrian.latorre@gmail.com>
2022-08-08 15:17:28 -0300

    AgregamosMockDeTareas

---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,32 @@
+import {Task} from "./Task"
+
+export const TASKS:Task[] = [
+{
+  id:1,
+  text:"Terminar modulo 1",
+  day: "Agosto 10 a las 12:00 ",
+  reminder: true
+},
+
+{
+  id:1,
+  text:"Llamar médico",
+  day: "Agosto 9 a las 11:00 ",
+  reminder: true
+},
+{
+  id:1,
+  text:"Investigar Firebase",
+  day: "Agosto 11 a las 14:00 ",
+  reminder: true
+},
+
+{
+  id:1,
+  text:"Buscar a Meli",
+  day: "Agosto 9 a las 20:00 ",
+  reminder: true
+},
+
+
+]
